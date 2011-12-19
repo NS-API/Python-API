d4171f583950a394ab435b300755846b4732174e
Stefan de Konink <stefan@konink.de>
2011-12-19 03:52:04 +0100

    Storingen implementatie in een nette klasse, kon nog niet getest worden, er zijn geen storingen :(

--- nsapi/storingen.py
+++ nsapi/storingen.py
@@ -1,86 +1,64 @@
 from xml.etree import cElementTree as ET
-from time import time, mktime, strftime, localtime
-from sleekxmpp.plugins.xep_0060.stanza.pubsub_event import Event, EventItems, EventItem, EventRetract
 
+import dateutil.parser
 import httplib2
 
-class NSAPIStoringen():
-    def __init__(self, username, password, xmpp=None):
-        user_agent = 'nsapi_pubsub/0.1'
-        self.headers = { 'User-Agent' : user_agent, \
-                         'Accept-Encoding': 'gzip, deflate, compress;q=0.9', \
-                         'Keep-Alive': '300', 'Connection': 'keep-alive', \
-                         'Cache-Control': 'max-age=0' }
-        self.h = httplib2.Http()
-        self.h.follow_redirects = False
-        self.h.add_credentials(username, password)
+def besteffort(xml, tag):
+    text = xml.find(xml.find(tag))
+    if text is not None:
+        if text.text == '':
+            return None
+        else:
+            return text.text
 
-        self.xmpp = xmpp
+class ongeplandestoring():
+    def __init__(self, xml):
+        self.id = xml.find('{http://openov.nl/protocol/nsapi}id').text
+        self.traject = xml.find('{http://openov.nl/protocol/nsapi}Traject').text
+        self.reden = xml.find('{http://openov.nl/protocol/nsapi}Reden').text
+        self.bericht = xml.find('{http://openov.nl/protocol/nsapi}Bericht').text
+        self.datum = dateutil.parser.parse(xml.find('{http://openov.nl/protocol/nsapi}VertrekTijd').text)
 
-        self.storingen = {} # storingen
+class geplandestoring():
+    def __init__(self, xml):
+        self.id = xml.find('{http://openov.nl/protocol/nsapi}id').text
+        self.traject    = besteffort(xml, '{http://openov.nl/protocol/nsapi}Traject')
+        self.periode    = besteffort(xml, '{http://openov.nl/protocol/nsapi}Periode')
+        self.reden      = besteffort(xml, '{http://openov.nl/protocol/nsapi}Reden')
+        self.advies     = besteffort(xml, '{http://openov.nl/protocol/nsapi}Advies')
+        self.bericht    = besteffort(xml, '{http://openov.nl/protocol/nsapi}Bericht')
+        self.oorzaak    = besteffort(xml, '{http://openov.nl/protocol/nsapi}Oorzaak')
+        self.vertraging = besteffort(xml, '{http://openov.nl/protocol/nsapi}Vertraging')
 
-    def query(self, code):
-        self.fetch([code])
-        if code in self.storingen:
-            return self.storingen[code]['storingen']
+class storingen():
+    def __init__(self, username, password):
+        self.http = httplib2.Http()
+        self.http.follow_redirects = False
+        self.http.add_credentials(username, password)
+
+    def fetch(self, station=None, actual=None, unplanned=None):
+        query = ''
+        if station is not None:
+            query += 'station=%s' % (station)
         
-        return None
+        if actual is not None:
+            query += 'actual=%s' % (str(actual).lower())
+        
+        if unplanned is not None:
+            query += 'unplanned=%s' % (str(unplanned).lower())
 
-    def fetch(self, codes, cache=True):
-        """
-        <Storingen>
-          <Ongepland>
-            <Storing>
-              <id>prio-13345</id>
-              <Traject>'s-Hertogenbosch-Nijmegen</Traject>
-              <Reden>beperkingen op last van de politie</Reden>
-              <Bericht></Bericht>
-              <Datum>2010-12-16T11:16:00+0100</Datum>
-            </Storing>
-          </ongepland>
-          <Gepland>
-            <Storing>
-              <id>2010_almo_wp_18_19dec</id>
-              <Traject>Almere Oostvaarders-Weesp/Naarden-Bussum</Traject>
-              <Periode>zaterdag 18 en zondag 19 december</Periode>
-              <Reden>Beperkt treinverkeer, businzet en/of omreizen, extra reistijd 15-30 min.</Reden>
-              <Advies>Maak gebruik van de overige treinen of de bussen: 
-                reis tussen Weesp en Almere Centrum met de NS-bus in
-                plaats van de trein tussen Almere Centrum en Lelystad 
-                Centrum rijden vier Sprinters per uur reis tussen Almere
-                Muziekwijk en Naarden-Bussum via Weesp</Advies>
-              <Bericht></Bericht>
-            </Storing>
-          </Gepland>
-        </Storingen>
-        """
+        resp, content = self.http.request("https://webservices.ns.nl/ns-api-storingen?%s" % (query))
+        if resp['status'] == '200':
+            content = content.replace('\n', '').replace('\t', '').replace('   ','').replace('  ', '')
+            try:
+                return ET.XML(content.replace('<Storingen>', '<Storingen xmlns="http://openov.nl/protocol/nsapi">'))
+            except:
+                pass
 
-        now = int(time())
-        for code in codes:
-            if cache and code in self.storingen and self.storingen[code]['expire'] > now:
-                continue
+    def fetchandparse(self, station=None, actual=None, unplanned=None):
+        root = self.fetch(station, actual, unplanned)
+        if root is not None:
+            ongepland = [ongeplandestoring(xmlstoring) for xmlstoring in root.findall('{http://openov.nl/protocol/nsapi}Ongepland/{http://openov.nl/protocol/nsapi}Storing')]
+            gepland   = [  geplandestoring(xmlstoring) for xmlstoring in root.findall('{http://openov.nl/protocol/nsapi}Gepland/{http://openov.nl/protocol/nsapi}Storing')]
 
-            resp, content = self.h.request("https://webservices.ns.nl/ns-api-storingen?station=%s&actueel=true&unplanned=true"%(code))
-            if resp['status'] == '200':
-                root = ET.XML(content.replace('\n', '').replace('\t', '').replace('    ', ''))
-                self.storingen[code] = {}
-                self.storingen[code]['expire'] = now + 600
-                self.storingen[code]['storingen'] = {}
-                self.storingen[code]['storingen']['gepland'] = {}
-                self.storingen[code]['storingen']['actueel'] = {}
-
-                for elem in root.findall('.//Gepland/Storing'):
-                    id = elem.find('.//id')
-                    if id is not None:
-                        self.storingen[code]['storingen']['gepland'][id.text] = elem
-                   
-                for elem in root.findall('.//Ongepland'):
-                    id = elem.find('.//id')
-                    if id is not None:
-                        self.storingen[code]['storingen']['actueel'][id.text] = elem
- 
-if __name__ == '__main__':
-    from secret import username, password
-    import sys
-    storingen = NSAPIStoringen(username, password)
-    print storingen.query(sys.argv[1])
+            return (ongepland + gepland)
